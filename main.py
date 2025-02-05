4e7f8b7a1dab7ac28af89dc93fb6a1fcf091940f
Sacha Ben <117613753+Bens368@users.noreply.github.com>
2025-02-05 16:53:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import openai
 import streamlit as st
 import os
-from moviepy.editor import VideoFileClip
+from moviepy import *
 
 # Configuration de l'API OpenAI
 openai.api_key = os.getenv("OPENAI_API_KEY")
